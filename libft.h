aac51f41d5a2a4dd61c3b0a6557299a81524f342
ngudiablo <nicolasgualtieriatome@gmail.com>
2025-06-17 16:45:01 +0200

    Add files via upload

--- libft.h
+++ libft.h
@@ -1,3 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngualtie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/17 14:51:36 by ngualtie          #+#    #+#             */
+/*   Updated: 2025/06/17 16:15:09 by ngualtie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 
 #ifndef LIBFT_H
 # define LIBFT_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+int     ft_atoi(const char *str);
+void    ft_bzero(void *s, size_t n);
+int     ft_isalpha(int c);
+int     ft_isascii(int c);
+int     ft_isdigit(int c);
+int     ft_isprint(int c);
+void    *ft_memchr(const void *s, int c, size_t n);
+void    *ft_memchr(const void *s, int c, size_t n);
+void    *ft_memcpy(void *dst, const void *src, size_t n);
+void    *ft_memmove(void *dst, const void *src, size_t len);
+void    *ft_memset(void *b, int c, size_t len);
+char    *ft_strchr(const char *s, int c);
+size_t  ft_strlcat(char *d, const char *s, size_t dstsize);
+size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *str);
+int     ft_strncmp(char *s1, char *s2, unsigned int n);
+char    *ft_strrchr(const char *s, int c);
+int     ft_tolower(int c);
+int     ft_toupper(int c);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
+int     ft_isalnum(int c);
+
+#endif
