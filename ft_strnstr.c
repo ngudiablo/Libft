f6fab22a29236aca9a029ee956781708e4e478ec
ngudiablo <nicolasgualtieriatome@gmail.com>
2025-06-17 21:53:17 +0200

    Add files via upload

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,11 +1,21 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ngualtie <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/17 17:29:25 by ngualtie          #+#    #+#             */
-/*   Updated: 2025/06/17 17:29:37 by ngualtie         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	a;
+	size_t	b;
+
+	a = 0;
+	if (!*little)
+		return ((char *)big);
+	while (big[a] && a < len)
+	{
+		b = 0;
+		while (little[b] && big[a + b] == little[b] && (a + b) < len)
+			b++;
+		if (!little[b])
+			return ((char *)&big[a]);
+		a++;
+	}
+	return (0);
+}
