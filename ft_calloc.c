1521b0a09394d95193e38bf294d35e094783e708
ngudiablo <nicolasgualtieriatome@gmail.com>
2025-06-18 21:55:58 +0200

    Add files via upload

---
+++ ft_calloc.c
@@ -0,0 +1,25 @@
+#include "libft.h"
+
+static void	*ft_memalloc(size_t size)
+{
+    void	*new;
+    size_t	a;
+
+    new = (void *) malloc(size);
+    if (!new)
+        return (0);
+    a = 0;
+    while (a < size)
+    {
+        *(unsigned char *)(new + a) = 0;
+        a++;
+    }
+    return (new);
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+    if (size != 0 && count > ((size_t) -1 / size))
+        return (NULL);
+    return (ft_memalloc(count * size));
+}
