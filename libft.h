9ec6a335fcc3685e273fba8fc9d365cef503c7d9
ngudiablo <nicolasgualtieriatome@gmail.com>
2025-06-22 20:06:33 +0200

    Add files via upload

--- libft.h
+++ libft.h
@@ -40,4 +40,9 @@ int		ft_isalnum(int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putendl_fd(char *s, int fd);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 #endif
